5c1031e194517149e0a91453e23438dabe9e725d
Danilo1dev <danilofoxlog@gmail.com>
2022-10-16 08:13:42 -0300

    atualiza atributo na entidade cliente e seus packages

--- src/main/java/br/com/naturaves/cobrancanaturaves/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/naturaves/cobrancanaturaves/cliente/application/service/ClienteApplicationService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteListResponse;
+import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteRequest;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteResponse;
-import br.com.naturaves.cobrancanaturaves.cliente.application.api.clienteRequest;
 import br.com.naturaves.cobrancanaturaves.cliente.application.repository.ClienteRepository;
 import br.com.naturaves.cobrancanaturaves.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ public class ClienteApplicationService implements ClienteService {
 	private final ClienteRepository clienteRepository;
 
 	@Override
-	public ClienteResponse criaCliente(clienteRequest clienteRequest) {
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaCliente");
 		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService - criaCliente");
--- src/main/java/br/com/naturaves/cobrancanaturaves/cliente/domain/Cliente.java
+++ src/main/java/br/com/naturaves/cobrancanaturaves/cliente/domain/Cliente.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteAlteracaoRequest;
-import br.com.naturaves.cobrancanaturaves.cliente.application.api.clienteRequest;
+import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteRequest;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,41 +29,42 @@ public class Cliente {
 	private UUID idCliente;
 
 	@NotBlank
-	@Column(unique = true)
-	private String codCliente;
-	@NotBlank
-	@Column(unique = true)
-	private String razaoSocial;
-	@Enumerated(EnumType.STRING)
-	private InscricaoSocial inscricaoSocial;
-	@NotBlank
-	private String telefone;
-	@Email
-	private String email;
-	@NotBlank
-	private String vendedor;
-	@NotNull
-	private LocalDate dataDoCadastro;
+    @Column(unique = true)
+    private String cliente;
+    @NotBlank
+    @Column(unique = true)
+    private String nomeCliente;
+    @Enumerated(EnumType.STRING)
+    private InscricaoSocial inscricaoSocial;
+    @NotBlank
+    private String telefone;
+    @Email
+    private String email;
+    @NotBlank
+    private String nomeVendedor;
+    @NotNull
+    private LocalDate dataDoCadastro;
 
-	private LocalDateTime dataHoraDaCadastro;
-	private LocalDateTime dataHoraDoUltimaAlteracao;
+    private LocalDateTime dataHoraDaCadastro;
+    private LocalDateTime dataHoraDoUltimaAlteracao;
 
-	public Cliente(clienteRequest clienteRequest) {
-		this.codCliente = clienteRequest.getCodCliente();
-		this.razaoSocial = clienteRequest.getRazaoSocial();
-		this.inscricaoSocial = clienteRequest.getInscricaoSocial();
-		this.telefone = clienteRequest.getTelefone();
-		this.email = clienteRequest.getEmail();
-		this.vendedor = clienteRequest.getVendedor();
-		this.dataDoCadastro = clienteRequest.getDataDoCadastro();
-		this.dataHoraDaCadastro = LocalDateTime.now();
-	}
+    public Cliente(ClienteRequest clienteRequest) {
+        this.cliente = clienteRequest.getCliente();
+        this.nomeCliente = clienteRequest.getNomeCliente();
+        this.inscricaoSocial = clienteRequest.getInscricaoSocial();
+        this.telefone = clienteRequest.getTelefone();
+        this.email = clienteRequest.getEmail();
+        this.nomeVendedor = clienteRequest.getNomeVendedor();
+        this.dataDoCadastro = clienteRequest.getDataDoCadastro();
+        this.dataHoraDaCadastro = LocalDateTime.now();
+    }
 
-	public void altera(ClienteAlteracaoRequest clienteAlteracaoRequest) {
-		this.inscricaoSocial = clienteAlteracaoRequest.getInscricaoSocial();
-		this.telefone = clienteAlteracaoRequest.getTelefone();
-		this.email = clienteAlteracaoRequest.getEmail();
-		this.vendedor = clienteAlteracaoRequest.getVendedor();
-		this.dataHoraDoUltimaAlteracao = LocalDateTime.now();
-	}
+    public void altera(ClienteAlteracaoRequest clienteAlteracaoRequest) {
+        this.inscricaoSocial = clienteAlteracaoRequest.getInscricaoSocial();
+        this.telefone = clienteAlteracaoRequest.getTelefone();
+        this.email = clienteAlteracaoRequest.getEmail();
+        this.nomeVendedor = clienteAlteracaoRequest.getNomeVendedor();
+        this.dataHoraDoUltimaAlteracao = LocalDateTime.now();
+    }
 }
+
