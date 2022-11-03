0c89c99805cc81cd8b1151544360fc53329622b0
Alex-Victor <alexvictoribeiro@gmail.com>
2022-11-03 17:25:58 -0300

    cria implementacao da interface TituloRepository

---
+++ src/main/java/br/com/naturaves/cobrancanaturaves/boleto/infra/TituloInfraRepository.java
@@ -0,0 +1,16 @@
+package br.com.naturaves.cobrancanaturaves.boleto.infra;
+
+import java.util.List;
+
+import br.com.naturaves.cobrancanaturaves.boleto.application.repository.TituloRepository;
+import br.com.naturaves.cobrancanaturaves.boleto.domain.Boleto;
+
+public class TituloInfraRepository implements TituloRepository {
+
+	@Override
+	public List<Boleto> buscaBoletoVencido(String nomeVendedor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
