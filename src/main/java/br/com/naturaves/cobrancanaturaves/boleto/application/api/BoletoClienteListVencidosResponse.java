e301942ea134b2fa82a8d1b19e198f612d00dc2d
Alex-Victor <alexvictoribeiro@gmail.com>
2022-11-08 23:40:58 -0300

    implementa filtro por dataVencimento

--- src/main/java/br/com/naturaves/cobrancanaturaves/boleto/application/api/BoletoClienteListVencidosResponse.java
+++ src/main/java/br/com/naturaves/cobrancanaturaves/boleto/application/api/BoletoClienteListVencidosResponse.java
@@ -13,39 +13,40 @@ import lombok.Value;
 
 @Value
 public class BoletoClienteListVencidosResponse {
-	
-		private UUID idBoleto;
-	    private String documento;
-	    private String parcela;
-	    private LocalDate dataVencimento;
-	    private BigDecimal saldoDevedor;
-	    private GrupoEmpresarial grupoEmpresarial;
-	    private LocalDateTime dataHoraDaCadastro;
-	
-	    public static List<BoletoClienteListVencidosResponse> converte(List<Boleto> boletoVencido) {
-	    	List<Boleto> boletosFiltrados = boletoVencido.stream().filter(boleto -> {
-	    		boolean igualMaiorQueDoisDias = boleto.getDataVencimento().isAfter(boleto.getDataVencimento().plusDays(2))
-	    				|| boleto.getDataVencimento().isEqual(boleto.getDataVencimento().plusDays(2));
-	    		if (igualMaiorQueDoisDias) return true; 
-	    			return false;
-	    	})
-	    			.collect(Collectors.toList());
-	    				
-			return boletosFiltrados
-					.stream()
-					.map(BoletoClienteListVencidosResponse::new)
-					.collect(Collectors.toList());
-		}
 
-		public BoletoClienteListVencidosResponse(Boleto boleto) {
-			this.idBoleto = boleto.getIdBoleto();
-			this.documento = boleto.getDocumento();
-			this.parcela = boleto.getParcela();
-			this.dataVencimento = boleto.getDataVencimento();
-			this.saldoDevedor = boleto.getSaldoDevedor();
-			this.grupoEmpresarial = boleto.getGrupoEmpresarial();
-			this.dataHoraDaCadastro = boleto.getDataHoraDaCadastro();
-		}
+	private UUID idBoleto;
+	private String documento;
+	private String parcela;
+	private LocalDate dataVencimento;
+	private BigDecimal saldoDevedor;
+	private GrupoEmpresarial grupoEmpresarial;
+	private LocalDateTime dataHoraDaCadastro;
 
-	    
+	public static List<BoletoClienteListVencidosResponse> converte(List<Boleto> boletoVencido) {
+		List<Boleto> boletosFiltrados = boletoVencido.stream().filter(boleto -> {
+			boolean igualMaiorQueDoisDias = boleto.getDataVencimento().plusDays(2).isAfter(LocalDate.now())
+					|| boleto.getDataVencimento().plusDays(2).isEqual(LocalDate.now());
+
+			if (igualMaiorQueDoisDias) {
+				return true;
+
+			} else {
+
+				return false;
+			}
+		}).collect(Collectors.toList());
+
+		return boletosFiltrados.stream().map(BoletoClienteListVencidosResponse::new).collect(Collectors.toList());
+	}
+
+	public BoletoClienteListVencidosResponse(Boleto boleto) {
+		this.idBoleto = boleto.getIdBoleto();
+		this.documento = boleto.getDocumento();
+		this.parcela = boleto.getParcela();
+		this.dataVencimento = boleto.getDataVencimento();
+		this.saldoDevedor = boleto.getSaldoDevedor();
+		this.grupoEmpresarial = boleto.getGrupoEmpresarial();
+		this.dataHoraDaCadastro = boleto.getDataHoraDaCadastro();
+	}
+
 }
